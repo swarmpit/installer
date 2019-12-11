e08e95b25802e02ea89ff4ff6dcb3f406552632d
Lumir Mrkva <mrq@mrq.cz>
2019-12-11 05:10:42 +0100

    deploy script fix

--- deploy-image.sh
+++ deploy-image.sh
@@ -19,7 +19,6 @@ fi
 
 IMAGE=$NAMESPACE:$TAG
 
-lein uberjar
 docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3
 docker buildx create --name multi
 docker buildx use multi
